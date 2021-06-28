711d4205daebaf008bb0b02129d3e961d78481e4
Lemon <Lemon123456@qq.com>
2021-06-28 18:44:03 +0800

    first commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -4,6 +4,7 @@
  */
 public class hello {
     public static void main(String[] args) {
-        System.out.println("cao");
+        System.out.println("cao0gggg");
+        System.out.println("Sdsds ");
     }
 }
